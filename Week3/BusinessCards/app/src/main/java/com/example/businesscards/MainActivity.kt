da2c94cb316cff8fe7f8184051f2c753d6149811
Warren Woellhaf <wmwprogrammer@gmail.com>
2020-06-20 10:57:12 -0700

    Fixed requested changes to meet the requirements

--- Week3/BusinessCards/app/src/main/java/com/example/businesscards/MainActivity.kt
+++ Week3/BusinessCards/app/src/main/java/com/example/businesscards/MainActivity.kt
@@ -4,20 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
     private val reminders = listOf("V = I x R", "I = V / R", "R = V / I", "P = V x I")
 
-    internal lateinit var currentReminder: String
-    private lateinit var changeReminderButton: Button
-    private lateinit var reminderTextView: TextView
-
-    private lateinit var reminderText: String
-    private lateinit var initialReminderText: String
+//    private var currentReminder: String = ""
+    private var reminderText: String = ""
+//    private var initialReminderText: String = ""
 
     companion object {
         private val TAG = MainActivity::class.java.simpleName
@@ -28,19 +24,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        changeReminderButton = findViewById(R.id.changeReminderButton)
-        reminderTextView = findViewById(R.id.reminderTextView)
-
         changeReminderButton.setOnClickListener {
             reminderText = reminders.random()
             reminderTextView.text = reminderText
         }
 
-        if (savedInstanceState != null) {
-            reminderText = savedInstanceState.getString(REMINDER_KEY).toString()
-        } else {
-            reminderText = reminders.random()
-        }
+        reminderText = savedInstanceState?.getString(REMINDER_KEY) ?: reminders.random()
         reminderTextView.text = reminderText
     }
 
@@ -52,17 +41,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.actionAbout) {
-            showInfo()
+            showBuildInfoDialog()
         }
         return true
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
         outState.putString(REMINDER_KEY, reminderText)
+        super.onSaveInstanceState(outState)
     }
 
-    private fun showInfo() {
+    private fun showBuildInfoDialog() {
         val dialogTitle = getString(R.string.aboutTitle)
         val dialogMessage = getString(R.string.aboutMessage)
         val buildVersion = BuildConfig.VERSION_NAME
